01248700f8b3dd4c1b34f108c0f6779bd6391929
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2017-05-20 11:20:25 +0900

    HTMLのa hrefの部分を取り出す処理を追加した

--- src/main/scala/com/github/mituba/crawler/Main.scala
+++ src/main/scala/com/github/mituba/crawler/Main.scala
@@ -1,6 +1,16 @@
 package com.github.mituba.crawler
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 object Main{
-  def main(args:Array[String]) = println("helloMain")
+  def getDocument(html:String):Document = Jsoup.parse(html)
+  def getUrlDocument(url:String):Document = Jsoup.parse(Jsoup.connect(url).userAgent("Mozilla").get().outerHtml())
+  def getFilterTaga() = getUrlDocument("http://dropbooks.tv/tops/index/sort:download_count/direction:desc/term:24h/page:1")
+      .select("a[href]")
+  def main(args:Array[String]){
+    getFilterTaga().toString().split("\n").filter { x => x.contains("/detail/") }.foreach { x => println(x) }
+//    println(getDocument((getFilterTaga())))
+  }
 }
